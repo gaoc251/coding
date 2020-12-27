d0da43cffc367d3cef43c1f8ff999160145a6289
gaochong01 <gaochong01@58.com>
2020-12-27 14:35:41 +0800

    mod

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,23 +1,4 @@
-const test = [98, 42, 25, 54, 15, 3, 25, 72, 41, 10, 121];
-
 function quickSort(array) {
-    if (array.length <=1) return array;
-    let left = [], right = []
-    let prvoitIndex = Math.floor(array.length/2);
-    let prvoit = array.splice(prvoitIndex, 1)[0];
-    array.forEach(item => {
-        if (item < prvoit) {
-            left.push(item)
-        } else {
-            right.push(item)
-        }
-    });
-    return quickSort(left).concat(prvoit, quickSort(right))
-}
-
-console.log("res", quickSort(test))
-
-// function quickSort(array) {
 //     if (array.length <=1) return array;
 //     const privotIndex = Math.floor(array.length/2);
 //     const privot = array.splice(privotIndex, 1)[0];
