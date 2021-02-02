b3c786cbb91fefa728365718e6d00ee029a7f94e
gaochong01 <gaochong01@58.com>
2021-02-02 12:05:05 +0800

    add

--- 寻找最长公共子串.js
+++ 寻找最长公共子串.js
@@ -70,3 +70,36 @@ function LCS(str1, str2){
 var str1="abcdefg";
 var str2="xyzabcd";
 console.log("lcs",LCS(str1, str2)  )   // abcd
+
+
+
+function longestCommonSubsequence(wordX, wordY) {
+    let m = wordX.length;
+    let n = wordY.length;
+    let solution = [];
+    //初始化一个二维数组，长度宽度分别为两个字符串的长度+1,内容为动态规划当前两分串的最长公共
+    for (let i = 0; i <= m; ++i) {
+      solution[i] = []; //{1}
+      for (let j = 0; j <= n; ++j) {
+        solution[i][j] = ""; //{2}
+      }
+    }
+    for (let i = 1; i <= m; i++) {
+      for (let j = 1; j <= n; j++) {
+        if (wordX[i - 1] == wordY[j - 1]) {
+          //因为字母长度少一位，我们二维数组从1，1开始用
+          //如果正好相同取左上方+1
+          //仅有这种情况的时候才会根据对角线添加一个字符
+          solution[i][j] = solution[i-1][j-1] + wordX[i-1]; //{3}
+        } else {
+          //如果不同分情况取左或上复制过来
+          let a = solution[i - 1][j].length;
+          let b = solution[i][j - 1].length;
+          solution[i][j] = a > b ? solution[i-1][j] : solution[i][j - 1]; //{4}
+        }
+      }
+    }
+    console.log(solution[m][n])
+    return solution[m][n].length;
+  }
+  console.log(longestCommonSubsequence("1A2C3D4B56","B1D23CA45B6A"));
